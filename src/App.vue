33b8d193f51fe3174bcdb935fc550769a3f6053f
Hidetada-komatu <2543144515@qq.com>
2018-03-18 21:19:22 +0800

    4.6 计算属性 <- Vuex状态

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div id="app" v-cloak>
-    <button @click="add">新建备忘</button>
-    <span>{{active | json}}</span>
-    <ul>
-      <li v-for="note in notes">{{note.text}}</li>
-    </ul>
+    <ez-note-list></ez-note-list>
+    <ez-note-editor></ez-note-editor>
   </div>
 </template>
 
@@ -21,7 +18,7 @@
     },
     mutations:{
       NEW_NOTE(state,note){
-        state.notes.unshift(note);
+        state.notes.push(note);
       },
       ACTIVATE_NOTE(state,note){
         state.active = note;
@@ -29,27 +26,94 @@
     }
   });
 
-  export default {
-    name: 'App',
-    store:store,
-    computed:{
-      notes(){ return this.$store.state.notes},
-      active(){return this.$store.state.active}
-    },
+  const EzNoteList = {
+    template:'#tpl-list',
+    computed:Vuex.mapState({
+      notes:state => state.notes,
+      active: state => state.active
+    }),
     methods:{
       add(){
-        let note = { text: '备忘 - ' + (this.notes.length + 1) + '#'};
+        let note = {
+          text: '备忘 - ' + (this.notes.length + 1) + '#'
+        };
         this.$store.commit('NEW_NOTE',note);
+        this.activate(note);
+      },
+      activate(note){
+        this.$store.commit('ACTIVATE_NOTE',note);
       }
     },
     filters:{
-      json(v){ return JSON.stringify(v)}
+      trim(v){
+        return v.trim();
+      },
+      slice(v,start,end){
+        return v.slice(start,end);
+      }
     }
+  };
+
+  const EzNoteEditor = {
+    template:'#tpl-editor',
+    computed:Vuex.mapState({note: 'active'})
+  };
+
+  export default {
+    name: 'App',
+    store:store,
+    components:{ EzNoteList,EzNoteEditor }
   }
 
 </script>
 
 <style>
+  html,body,#app{
+    height:100%;
+    padding:0;
+    margin:0;
+    overflow:hidden;
+  }
+  #app{
+    display:flex;
+  }
+  .note-list{
+    width:230px;
+    display:flex;
+    flex-direction:column;
+    background:#fafafa;
+  }
+  .note-list .toolbar{
+    height:30px;
+    line-height:30px;
+    border-bottom:1px solid #ccc;
+  }
+  .note-list ul{
+    flex:1;
+    list-style:none;
+    padding:0;
+    margin:0;
+    overflow-y:auto;
+  }
+  .note-list li{
+    cursor:pointer;
+    padding:5px;
+  }
+  .note-list li.active{
+    background:blue;
+    color:white;
+  }
+  .note-editor{
+    flex:1;
+  }
+  .note-editor textarea{
+    width:100%;
+    height:100%;
+    resize:none;
+    outline:none;
+    border:none;
+    border-left:1px solid #ccc;
+  }
   [v-cloak]:after{
     content:' ';
     display:block;
