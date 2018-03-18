5ee34ffdf70f1216cd53bbad509184ce0ba1d0b9
Hidetada-komatu <2543144515@qq.com>
2018-03-18 21:17:31 +0800

    4.5 状态变更管理

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app" v-cloak>
-    <ez-note-list></ez-note-list>
-    <ez-note-editor></ez-note-editor>
+    <button @click="add">新建备忘</button>
+    <span>{{active | json}}</span>
+    <ul>
+      <li v-for="note in notes">{{note.text}}</li>
+    </ul>
   </div>
 </template>
 
@@ -15,105 +18,38 @@
     state:{
       notes:[],
       active:{}
+    },
+    mutations:{
+      NEW_NOTE(state,note){
+        state.notes.unshift(note);
+      },
+      ACTIVATE_NOTE(state,note){
+        state.active = note;
+      }
     }
   });
-
-  const EzNoteList = {
-    template:'#tpl-list',
-    computed:{
-      notes:{
-        get(){ return this.$store.state.notes;},
-        set(v){ this.$store.state.notes = v;}
-      },
-      active:{
-        get(){ return this.$store.state.active;},
-        set(v){ this.$store.state.active = v;}
-      }
-    },
-    methods:{
-      add(){
-        let note = {
-          text: '备忘 - ' + (this.notes.length + 1) + '#'
-        };
-        this.notes.unshift(note);
-        this.active = note;
-      }
-    },
-    filters:{
-      trim(v){
-        return v.trim();
-      },
-      slice(v,start,end){
-        return v.slice(start,end);
-      }
-    }
-  };
-
-  const EzNoteEditor = {
-    template:'#tpl-editor',
-    computed:{
-      note:{
-        get(){ return this.$store.state.active;},
-        set(v){ this.$store.state.active = v;}
-      }
-    }
-  };
 
   export default {
     name: 'App',
     store:store,
-    components:{ EzNoteList,EzNoteEditor }
+    computed:{
+      notes(){ return this.$store.state.notes},
+      active(){return this.$store.state.active}
+    },
+    methods:{
+      add(){
+        let note = { text: '备忘 - ' + (this.notes.length + 1) + '#'};
+        this.$store.commit('NEW_NOTE',note);
+      }
+    },
+    filters:{
+      json(v){ return JSON.stringify(v)}
+    }
   }
 
 </script>
 
 <style>
-  html,body,#app{
-    height:100%;
-    padding:0;
-    margin:0;
-    overflow:hidden;
-  }
-  #app{
-    display:flex;
-  }
-  .note-list{
-    width:230px;
-    display:flex;
-    flex-direction:column;
-    background:#fafafa;
-  }
-  .note-list .toolbar{
-    height:30px;
-    line-height:30px;
-    border-bottom:1px solid #ccc;
-  }
-  .note-list ul{
-    flex:1;
-    list-style:none;
-    padding:0;
-    margin:0;
-    overflow-y:auto;
-  }
-  .note-list li{
-    cursor:pointer;
-    padding:5px;
-  }
-  .note-list li.active{
-    background:blue;
-    color:white;
-  }
-  .note-editor{
-    flex:1;
-  }
-  .note-editor textarea{
-    width:100%;
-    height:100%;
-    resize:none;
-    outline:none;
-    border:none;
-    border-left:1px solid #ccc;
-  }
   [v-cloak]:after{
     content:' ';
     display:block;
